5df25454ce756e8282507f102b000276701e49d7
Afnan <amalotaibi@jahez.net>
2025-05-31 18:01:56 +0300

    Initial commit for ReadPop project

---
+++ ReadPop/ReadPopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadPopApp.swift
+//  ReadPop
+//
+//  Created by Afnan Alotaibi on 04/12/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ReadPopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
